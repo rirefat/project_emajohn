2fe3110e390b3013b2ec77e785db15681c8c5f37
rirefat <rirefat.official@gmail.com>
2022-08-14 23:37:35 +0600

    done navbar toggling option

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 
 const Header = () => {
+    const [open, setOpen]=useState(false);
     return (
         <nav className='navbar'>
             <img src={logo} alt="logo" />
@@ -12,6 +13,11 @@ const Header = () => {
                 <li><a href="#"><i class='bx bxs-shopping-bags icons' ></i>Order Review</a></li>
                 <li><a href="#"><i class='bx bxs-home-smile icons'></i>Manage Inventory</a></li>
             </ul>
+            <div className="menu-toggle-btn" onClick={()=>setOpen(!open)}>
+                {console.log(open)}
+                <i class={`bx bx-menu-alt-right ${open?"hidden":"show"}`}></i>
+                <i class={`bx bx-x ${!open?"hidden":"show"}`}></i>
+            </div>
         </nav>
     );
 };
