c90e6c711cc9803e24a9ae8c1910b6e8fb0b9e39
rirefat <rirefat.official@gmail.com>
2022-08-16 01:31:43 +0600

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import ScrollToTop from "react-scroll-to-top";
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <Header></Header>
       <Shop></Shop>
       <Footer></Footer>
+
+      <ScrollToTop smooth={true} top={500} width={18} height={18}/>
     </div>
   );
 };
