c8f66c824d9075b1e6afb2c6dd3819ae2732d759
rirefat <rirefat.official@gmail.com>
2022-08-16 02:03:21 +0600

    done calculation function

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -19,6 +19,24 @@ const Shop = () => {
 
     const clearCart = ()=>{setCart([]);}
 
+    // Price Calculation 
+    let total = 0;
+    for (const item of cart){
+        total = total + item.price;
+    }
+
+    // Shipping Charge Calculation
+    let shippingCost = 0;
+    for (const item of cart){
+        shippingCost = shippingCost+item.shipping;
+    }
+
+    // Tax Calculation
+    let tax = total*0.1;
+
+    // Total Calculation
+    let grandTotal = total+shippingCost+tax;
+
     return (
         <div className='shop-container'>
             <div className="product-container">
@@ -38,14 +56,15 @@ const Shop = () => {
                     <h1 className='section-title'>Order Summary</h1>
                     <div className="summery-content">
                         <p>Selected Items: {cart.length}</p>
-                        <p>Total Price: $</p>
-                        <p>Total Shipping Charge: $</p>
-                        <p>Tax: $</p>
-                        <h3>Grand Total: $</h3>
+                        <p>Total Price: $ {total}</p>
+                        <p>Total Shipping Charge: $ {shippingCost}</p>
+                        <p>Tax: $ {tax.toFixed(2)}</p>
+                        <h3>Grand Total: $ {grandTotal.toFixed(2)}</h3>
                         <div className="btn-section">
                             <button onClick={clearCart}><span>Clear Cart</span> <i className='bx bxs-trash' ></i></button>
                             <button><span>Review Order</span> <i className='bx bx-right-arrow-alt' ></i></button>
                         </div>
+                        <span className='short-note'>* Flat 10% Tax is Applicable</span>
                     </div>
                 </div>
             </div>
