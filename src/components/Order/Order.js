599a621a73bdfc739288c38a02a74ac169827c0b
rirefat <rirefat.official@gmail.com>
2022-09-05 17:57:19 +0600

    use custom hook for loading data

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import useProducts from '../customHooks/useProducts';
 import './Order.css';
 
 const Order = () => {
+    const [products, setProducts]=useProducts();
     return (
         <div>
             <h1>Order Page</h1>
+            <p>Available Products: {products.length}</p>
         </div>
     );
 };
